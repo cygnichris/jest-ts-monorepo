e2fbf7d5f6d8da7205251dffc704f09006df8b35
Chris Shields <christopher.shields@cygni.se>
2019-06-17 11:15:20 +0200

    Misspelt jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/packages/foo-packagea/jest.config.js', '<rootDir>/packages/foo-packageb/jest-config.js'],
+  projects: ['<rootDir>/packages/foo-packagea/jest.config.js', '<rootDir>/packages/foo-packageb/jest.config.js'],
 };
